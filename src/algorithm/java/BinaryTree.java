0df15da91b7d514af194a50891905baada372b36
tony <aihua53@163.com>
2019-12-01 22:51:40 +0800

    leetcode #101
    对称二叉树

--- src/algorithm/java/BinaryTree.java
+++ src/algorithm/java/BinaryTree.java
@@ -100,4 +100,26 @@ class BinaryTree{
 		}
 	    return false;
 	}
+	
+	/*
+	 * leetcode #101
+	 * ¶Ô³Æ¶þ²æÊ÷
+	 */
+	public static boolean isSymmetric(TreeNode root) {
+		return isMirror(root,root);
+	}
+	
+	public static boolean isMirror(TreeNode t1, TreeNode t2) {
+		if(t1 == null && t2 == null) {
+			return true;
+		}	
+		if(t1 ==null || t2 == null) {
+			return false;
+		}
+		if((t1.value == t2.value)&&isMirror(t1.left,t2.right)&&isMirror(t1.right,t2.left)) {
+			return true;
+		}
+		return false;
+		
+	}
 }
