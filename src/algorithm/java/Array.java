8e498406c47b0cbc91b54dc738ec9bf1df14241a
wangwei1 <wangwei1@chehejia.com>
2020-03-02 14:41:37 +0800

    leetcode #88
    merge array

--- src/algorithm/java/Array.java
+++ src/algorithm/java/Array.java
@@ -1,5 +1,6 @@
 package algorithm.java;
 
+import java.util.Arrays;
 import java.util.Vector;
 
 public class Array {
@@ -124,6 +125,31 @@ public class Array {
 		}
 		return a[n];
 	}
+
+
+	//leetcode #88
+	public static void merge(int[] nums1,int m, int[] nums2,int n) {
+		System.arraycopy(nums2, 0, nums1, m, n);
+		Arrays.parallelSort(nums1);
+
+	}
+
+	public static void merge_2(int[] nums1,int m, int[] nums2,int n) {
+	    // two get pointers for nums1 and nums2
+	    int p1 = m - 1;
+	    int p2 = n - 1;
+	    // set pointer for nums1
+	    int p = m + n - 1;
+
+	    // while there are still elements to compare
+	    while ((p1 >= 0) && (p2 >= 0))
+	      // compare two elements from nums1 and nums2
+	      // and add the largest one in nums1
+	      nums1[p--] = (nums1[p1] < nums2[p2]) ? nums2[p2--] : nums1[p1--];
+
+	    // add missing elements from nums2
+	    System.arraycopy(nums2, 0, nums1, 0, p2 + 1);
+	  }
 
 
 }
